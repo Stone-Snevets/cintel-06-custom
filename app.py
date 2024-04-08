74d7047bf55258792ac0da657b0989eedf399c7d
Stone-Snevets <142818150+Stone-Snevets@users.noreply.github.com>
2024-04-08 10:23:52 -0400

    Include rough sketch of app

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
+# Imports
+#from shiny import reactive, render
+#import pandas as pd #-> Load into pandas DataFrame
+#import shinywidgets #-> for chart(s)
 
+
+# === Read in File ============================================================================
+
+
+# UI page options
+
+# === UI Sidebar ==============================================================================
+
+# Choose at least one input user can interact with to filter data
+
+# === UI Main =================================================================================
+
+#-> Basic dashboard template
+
+    # Output text
+    #-> Cards
+
+    # Table / Grid
+
+    # Chart
+    #-> ipywidgits
+
+# --- Reactive Calc Function -------------------------------------------------------------------
+
+# Similar to project 3
